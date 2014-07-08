5c239c81175f9ff0dc63a0b927eee5c81adb45bc
johhag <john.haggkvist@postkodlotteriet.se>
2014-07-08 12:43:38 +0200

    Refactor app

---
+++ js/controller/timer.js
@@ -0,0 +1,59 @@
+angular.module("timerino.Timer", []).
+controller("TimerCtrl", ['$scope', '$document', function($scope, $document) {
+  var start = undefined,
+      holding = false, 
+      starting = false;
+  setTimed("T:im:er:ino", true);
+
+  function setTimed(value, doNotApply) {
+    $scope.timed = value;
+    if (!doNotApply) $scope.$apply();
+  }
+
+  $document.bind('keydown', function ($event) {
+    if ($event.which === 32) {
+      if (!holding) {
+        if (start) {
+          var timed = $event.timeStamp - start;
+          start = undefined;
+          holding = true;
+
+          setTimed(formatTime(timed));
+        } else if (!start) {
+          holding = true;
+          starting = true;
+          setTimed('Waiting...');
+        }
+      }
+    }
+  });
+
+  $document.bind('keyup', function ($event) {
+    console.log('up',$event, start, starting);
+    if ($event.which === 32) {
+      if (!start && starting) {
+        start = $event.timeStamp;
+        starting = false;
+        setTimed('Timing!');
+      }
+      holding = false;
+    }
+  });
+
+  function formatTime(millis) {
+    function _zeropad(num, len) {
+      num = '' + num;
+      while (num.length < len) {
+        num = '0' + num;
+      }
+      return num;
+    }
+    var seconds = Math.floor(millis / 1000);
+    millis = _zeropad(millis % 1000, 3);
+    var minutes = Math.floor(seconds / 60);
+    seconds = _zeropad(seconds % 60, 2);
+    var hours = Math.floor(minutes / 60);
+    minutes = _zeropad(minutes % 60, 2);
+    return (hours ? _zeropad(hours, 2) + ':' : '') + minutes + ':' + seconds + ':' + millis;
+  }
+}]);
--- js/timerino.js
+++ js/timerino.js
@@ -1,50 +1,15 @@
-var timerinoApp = angular.module("timerinoApp", []);
-    timerinoApp.controller("TimerCtrl", ['$scope', function($scope) {
-      var start = undefined,
-          holding = false, 
-          starting = false;
-     $scope.timed = "T:im:er:ino"
+angular.module('timerino.Timer', []);
 
-      function formatTime(millis) {
-        function _zeropad(num, len) {
-          num = '' + num;
-          while (num.length < len) {
-            num = '0' + num;
-          }
-          return num;
-        }
-        var seconds = Math.floor(millis / 1000);
-        millis = _zeropad(millis % 1000, 3);
-        var minutes = Math.floor(seconds / 60);
-        seconds = _zeropad(seconds % 60, 2);
-        var hours = Math.floor(minutes / 60);
-        minutes = _zeropad(minutes % 60, 2);
-        return (hours ? _zeropad(hours, 2) + ':' : '') + minutes + ':' + seconds + ':' + millis;
-      }
-
-      $scope.keydown = function ($event) {
-        if ($event.which === 32) {
-          if (!holding) {
-            if (start) {
-              $scope.timed = formatTime($event.timeStamp - start);
-              start = undefined;
-              holding = true;
-            } else if (!start) {
-              holding = true;
-              starting = true;
-              $scope.timed = 'Waiting...';
-            }
-          }
-        }
-      };
-      $scope.keyup = function ($event) {
-        if ($event.which === 32) {
-          if (!start && starting) {
-            start = $event.timeStamp;
-            starting = false;
-            $scope.timed = 'Timing!';
-          }
-          holding = false;
-        }
-      };
-    }]);
+angular.module('timerino', [
+  'timerino.Timer',
+  'ngRoute'
+]).
+config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.when('/timer', {
+      templateUrl: 'template/timer.html',
+      controller: 'TimerCtrl'
+  });
+  $routeProvider.otherwise({
+      redirectTo: '/timer'
+  });
+}]);
