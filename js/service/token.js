6a2721f0108d576033737713ad166f370658f262
johhag <john.haggkvist@postkodlotteriet.se>
2014-07-09 14:58:27 +0200

    Remove base64

--- js/service/token.js
+++ js/service/token.js
@@ -1,121 +1,42 @@
 angular.module("timerino.Token", []).
 factory("TokenService", [function() {
-  // Base64 from http://stackoverflow.com/questions/246801/how-can-you-encode-a-string-to-base64-in-javascript
-  var Base64 = {
-    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-
-    encode : function (input) {
-      var output = "";
-      var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-      var i = 0;
-
-      input = Base64._utf8_encode(input);
-
-      while (i < input.length) {
-        chr1 = input.charCodeAt(i++);
-        chr2 = input.charCodeAt(i++);
-        chr3 = input.charCodeAt(i++);
-
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
-
-        if (isNaN(chr2)) {
-          enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-          enc4 = 64;
-        }
-
-        output = output +
-        Base64._keyStr.charAt(enc1) + Base64._keyStr.charAt(enc2) +
-        Base64._keyStr.charAt(enc3) + Base64._keyStr.charAt(enc4);
-      }
-
-      return output;
-    },
-
-    decode : function (input) {
-      var output = "";
-      var chr1, chr2, chr3;
-      var enc1, enc2, enc3, enc4;
-      var i = 0;
-
-      input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-      while (i < input.length) {
-        enc1 = Base64._keyStr.indexOf(input.charAt(i++));
-        enc2 = Base64._keyStr.indexOf(input.charAt(i++));
-        enc3 = Base64._keyStr.indexOf(input.charAt(i++));
-        enc4 = Base64._keyStr.indexOf(input.charAt(i++));
-
-        chr1 = (enc1 << 2) | (enc2 >> 4);
-        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-        chr3 = ((enc3 & 3) << 6) | enc4;
-
-        output = output + String.fromCharCode(chr1);
-
-        if (enc3 != 64) {
-          output = output + String.fromCharCode(chr2);
-        }
-        if (enc4 != 64) {
-          output = output + String.fromCharCode(chr3);
-        }
-
-      }
-
-      output = Base64._utf8_decode(output);
-
-      return output;
-    },
-
-    _utf8_encode : function (string) {
-      string = string.replace(/\r\n/g,"\n");
-      var utftext = "";
-
-      for (var n = 0; n < string.length; n++) {
-        var c = string.charCodeAt(n);
-
-        if (c < 128) {
-          utftext += String.fromCharCode(c);
-        } else if((c > 127) && (c < 2048)) {
-          utftext += String.fromCharCode((c >> 6) | 192);
-          utftext += String.fromCharCode((c & 63) | 128);
-        } else {
-          utftext += String.fromCharCode((c >> 12) | 224);
-          utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-          utftext += String.fromCharCode((c & 63) | 128);
-        }
-      }
-
-      return utftext;
-    },
-
-    _utf8_decode : function (utftext) {
-      var string = "";
-      var i = 0;
-      var c = c1 = c2 = 0;
-
-      while ( i < utftext.length ) {
-        c = utftext.charCodeAt(i);
-
-        if (c < 128) {
-          string += String.fromCharCode(c);
-          i++;
-        } else if((c > 191) && (c < 224)) {
-          c2 = utftext.charCodeAt(i+1);
-          string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-          i += 2;
-        } else {
-          c2 = utftext.charCodeAt(i+1);
-          c3 = utftext.charCodeAt(i+2);
-          string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-          i += 3;
-        }
-      }
-      return string;
+  function buildMiniTimesToken(times) {
+    var minTimed = Number.MAX_VALUE;
+    var minWhen = Number.MAX_VALUE;
+    for (var i = 0; i < times.length; i++) {
+        if (times[i].timed < minTimed) minTimed = times[i].timed;
+        if (times[i].when < minWhen) minWhen = times[i].when;
     }
-  };
+    var output = ["t", new Date().getTime() - minWhen, minWhen, minTimed];
+    for (var i = 0; i < times.length; i++) {
+        output.push(times[i].when - minWhen);
+        output.push(times[i].timed - minTimed);
+    }
+    return output.join(",");
+  }
+
+  function parseMiniTimesToken(token) {
+    var input = token.split(",");
+    if (input.length < 4) return "invalid";
+
+    var type = input[0],
+        minWhen = parseInt(input[2], 10),
+        minTimed = parseInt(input[3], 10),
+        when = parseInt(input[1], 10) + minWhen;
+
+    var output = {
+        when: when,
+        times: []
+    };
+    for (var i = 4; i + 1 < input.length; i += 2) {
+        output.times.push({
+            when: parseInt(input[i], 10) + minWhen,
+            timed: parseInt(input[i + 1], 10) + minTimed
+        });
+    }
+
+    return output;
+  }
 
   var TokenService = {
     TYPE_TIMES: 'times',
@@ -123,14 +44,10 @@ factory("TokenService", [function() {
       return Base64.encode(something);
     },
     tokenTimes: function (times) {
-      return Base64.encode(JSON.stringify({
-        type: this.TYPE_TIMES,
-        times: times,
-        when: new Date().getTime()
-      }));
+      return buildMiniTimesToken(times);
     },
     unpackToken: function (token) {
-      return JSON.parse(Base64.decode(token));
+      return parseMiniTimesToken(token);
     }
   };
 
