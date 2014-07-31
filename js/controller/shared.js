1c61c7932b171dc386e81541a45b01610e14b5a8
johhag <john.haggkvist@postkodlotteriet.se>
2014-07-31 09:42:13 +0200

    Graphs

--- js/controller/shared.js
+++ js/controller/shared.js
@@ -1,5 +1,35 @@
 angular.module("timerino.Shared", ['timerino.Directives', 'timerino.Token', 'timerino.Times']).
-controller("SharedCtrl", ['$scope', '$routeParams', 'TokenService', 'TimesService', function($scope, $routeParams, TokenService, TimesService) {
+controller("SharedCtrl", ['$scope', '$routeParams', '$filter', 'TokenService', 'TimesService', function($scope, $routeParams, $filter, TokenService, TimesService) {
+  // export to grant graph js access to our filter :(
+  window.timerinoTimed = function (val) { return $filter('timed')(val); }
+
+  function updateCanvas(canvasId, times, reverse) {
+    var graphTimes = [],
+        graphWhens = [];
+    for (var i = 0; i < times.length; i++) {
+        var time = times[i];
+        if (reverse) {
+          graphTimes.unshift(time.timed);
+        } else {
+          graphTimes.push(time.timed);
+        }
+        graphWhens.push('');
+    }
+    var data = {
+      labels: graphWhens,
+      datasets: [{
+          data: graphTimes
+      }]
+    };
+
+    var ctx = document.getElementById(canvasId).getContext("2d");
+    var myNewChart = new Chart(ctx).Bar(data, {
+        scaleLabel: "<%=timerinoTimed(value)%>",
+        tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= timerinoTimed(value) %>",
+        animationSteps: 15
+    });
+  }
+
   if ($routeParams.token) {
     var data = TokenService.unpackToken($routeParams.token);
     $scope.times = data.times;
@@ -8,5 +38,8 @@ controller("SharedCtrl", ['$scope', '$routeParams', 'TokenService', 'TimesServic
     
     $scope.average = TimesService.average(data.times);
     $scope.competetiveAverage = TimesService.competetiveAverage(data.times);
+
+    updateCanvas('latestCanvas', data.times, true);
+    updateCanvas('bestCanvas', data.best);
   }
 }]);
